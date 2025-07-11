a5a072eb39533767b1413e0a64da1b87b1334fd9
PavelKozaev <pkozaev221299@gmail.com>
2025-07-11 09:35:58 +0300

    feat: Implement Application layer with CQRS commands

---
+++ TicTacToe/TicTacToe.Application/Features/Games/Commands/CreateGameCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using TicTacToe.Application.Interfaces;
+using TicTacToe.Domain.Entities;
+
+namespace TicTacToe.Application.Features.Games.Commands
+{
+    public record CreateGameCommand : IRequest<Guid>;
+
+    public class CreateGameCommandHandler : IRequestHandler<CreateGameCommand, Guid>
+    {
+        private readonly IGameRepository _gameRepository;
+
+        public CreateGameCommandHandler(IGameRepository gameRepository)
+        {
+            gameRepository = _gameRepository;
+        }
+
+        public async Task<Guid> Handle(CreateGameCommand request, CancellationToken cancellationToken)
+        {
+            const int boardSize = 3;
+            const int winCondition = 3;
+
+            var game = new Game(Guid.NewGuid(), boardSize, winCondition);
+
+            await _gameRepository.AddAsync(game, cancellationToken);
+
+            return game.Id;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe.Application/Features/Games/Commands/MakeMoveCommand.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using TicTacToe.Application.Interfaces;
+using TicTacToe.Domain.Enums;
+
+namespace TicTacToe.Application.Features.Games.Commands
+{
+    public record MakeMoveCommand(Guid GameId, Player Player, int Row, int Column) : IRequest<Unit>;
+
+    public class MakeMoveCommandHandler : IRequestHandler<MakeMoveCommand, Unit>
+    {
+        private readonly IGameRepository _gameRepository;
+        private readonly IRandomProvider _randomProvider;
+
+        public MakeMoveCommandHandler(IGameRepository gameRepository, IRandomProvider randomProvider)
+        {
+            _gameRepository = gameRepository;
+            _randomProvider = randomProvider;
+        }
+
+        public async Task<Unit> Handle(MakeMoveCommand request, CancellationToken cancellationToken)
+        {
+            var game = await _gameRepository.GetByIdAsync(request.GameId, cancellationToken);
+
+            if (game is null)
+            {
+                throw new ApplicationException($"Game with id {request.GameId} not found.");
+            }
+
+            if (game.CurrentPlayer != request.Player)
+            {
+                throw new InvalidOperationException($"It's not player {request.Player}'s turn.");
+            }
+
+            game.MakeMove(request.Row, request.Column, _randomProvider.ShouldOccur);
+
+            await _gameRepository.UpdateAsync(game, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
